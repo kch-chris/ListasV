011753d14923d202dc7ba06b37fda1c121f28585
Christian Murillo <christian.murillo@nuvemtecnologia.mx>
2013-06-21 19:29:31 -0500

    asd

--- js/API/almacenamiento.js
+++ js/API/almacenamiento.js
@@ -26,7 +26,6 @@ function accesoBD ()
 function EnviarTarea(titulo,descripcion,foto)
 {
 	var estado=1;
-	alert(titulo+descripcion+foto+estado);
 	accesoBD().trasaction(function(tx) {
 		 tx.executeSql('CREATE TABLE IF NOT EXISTS Tareas (id unique, Titulo, Descripcion, Foto, Estado)');
 		 tx.executeSql('INSERT INTO Tareas (Titulo, Descripcion, Foto, Estado) VALUES ("'+titulo+'", "'+descripcion+'", "'+foto+'", "'+estado+'")');
